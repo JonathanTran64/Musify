cc6a048d5d4b73b8ab198028d5d91d407ed07357
Jonathan <jonathantran2828@gmail.com>
2024-05-01 02:53:16 -0400

    created kpop fetch and home component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Kpop from "./Kpop";
+import Home from "./Home";
+
 const App = () => {
-  return <h1>hi</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kpop" element={<Kpop />} />
+        <Route path="*" element={<h1>404 page not found</h1>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;

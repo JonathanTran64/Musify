0d165445408adf22d9854ebdbf7dcaccf3cc2b4d
Jonathan <jonathantran2828@gmail.com>
2024-05-02 04:57:50 -0400

    styled the suggestion, play button, create nav

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const NavBar = () => {
+  return <Container></Container>;
+};
+const Container = styled.div`
+  width: 100%;
+  height: 60px;
+  margin-bottom: 20px;
+  background-color: black;
+`;
+export default NavBar;
--- client/src/components/SongGame.js
+++ client/src/components/SongGame.js
@@ -3,13 +3,16 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import FuzzySearch from "fuzzy-search";
 import GuessBox from "./GuessBox";
+import playButtonImage from "../assets/playbutton.png";
+import stopButtonImage from "../assets/stopbutton.png";
+import NavBar from "./NavBar";
 
 const SongGame = () => {
   const [song, setSong] = useState();
   const { genre } = useParams();
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [seconds, setSeconds] = useState(1000);
+  const [seconds, setSeconds] = useState(1);
   const [allSongs, setAllSongs] = useState([]);
   const [inputGuess, setInputGuess] = useState("");
   const [suggestions, setSuggestion] = useState([]);
@@ -62,26 +65,49 @@ const SongGame = () => {
     console.log(result);
   }, [inputGuess]);
 
+  // PRESS PLAY
   const handlePlay = () => {
     audioRef.current.currentTime = 0;
-    audioRef.current.play();
-    setIsPlaying(true);
 
-    setTimeout(() => {
+    if (!isPlaying) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    } else {
       audioRef.current.pause();
       setIsPlaying(false);
-    }, seconds);
+    }
   };
 
-  const handleSkip = () => {
-    if (count < 5000) {
-      const newTries = [...tries];
-      newTries[count / 1000] = "S K I P P E D";
-      setTries(newTries);
+  // AUTOMATIC PAUSE AT SPECIFIED SECONDS
+  useEffect(() => {
+    const handleTimeUpdate = () => {
+      if (audioRef.current && audioRef.current.currentTime >= seconds) {
+        audioRef.current.pause();
+        setIsPlaying(false);
+        audioRef.current.currentTime = 0;
+      }
+    };
+
+    if (audioRef.current) {
+      audioRef.current.addEventListener("timeupdate", handleTimeUpdate);
     }
 
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.removeEventListener("timeupdate", handleTimeUpdate);
+      }
+    };
+  }, [seconds, song]);
+
+  // PRESS SKIP
+  const handleSkip = () => {
+    if (count < 5) {
+      const newTries = [...tries];
+      newTries[count] = "S K I P P E D";
+      setTries(newTries);
+    }
     setSkip((skip += 1));
-    setCount((count += 1000));
+    setCount((count += 1));
     setSeconds(seconds + count);
   };
 
@@ -94,13 +120,13 @@ const SongGame = () => {
 
     if (
       inputGuess.trim() !== `${song.artistName} - ${song.songName}` &&
-      count < 5000
+      count < 5
     ) {
       const newTries = [...tries];
-      newTries[count / 1000] = inputGuess.trim();
+      newTries[count] = inputGuess.trim();
       setTries(newTries);
       setSkip((skip += 1));
-      setCount((count += 1000));
+      setCount((count += 1));
       setSeconds(seconds + count);
       setInputGuess("");
     } else {
@@ -111,76 +137,91 @@ const SongGame = () => {
 
   return (
     <Container>
+      <NavBar></NavBar>
       <Wrapper>
         {tries.map((tryAnswer, index) => (
           <GuessBox
             key={index}
-            active={count === index * 1000 ? "active" : "not-active"}
+            active={count === index ? "active" : "not-active"}
             answer={tryAnswer}
           />
         ))}
 
         {song ? (
           <>
-            <StyledAudio ref={audioRef} src={song.preview} />
+            <audio ref={audioRef} src={song.preview} />
 
             {/* AUDIO BAR */}
             <ProgressBar>
               <Skipped
-                $width={"45px"}
+                $width={"50px"}
                 $backColor={count >= 0 ? "lightgray" : "none"}
               ></Skipped>
               <Skipped
-                $width={"90px"}
-                $backColor={count >= 1000 ? "lightgray" : "none"}
+                $width={"95px"}
+                $backColor={count >= 1 ? "lightgray" : "none"}
               ></Skipped>
               <Skipped
-                $width={"175px"}
-                $backColor={count >= 2000 ? "lightgray" : "none"}
+                $width={"185px"}
+                $backColor={count >= 2 ? "lightgray" : "none"}
               ></Skipped>
               <Skipped
-                $width={"305px"}
-                $backColor={count >= 3000 ? "lightgray" : "none"}
+                $width={"315px"}
+                $backColor={count >= 3 ? "lightgray" : "none"}
               ></Skipped>
               <Skipped
-                $width={"480px"}
-                $backColor={count >= 4000 ? "lightgray" : "none"}
+                $width={"487px"}
+                $backColor={count >= 4 ? "lightgray" : "none"}
               ></Skipped>
               <Skipped
                 $width={"700px"}
-                $backColor={count >= 5000 ? "lightgray" : "none"}
+                $backColor={count >= 5 ? "lightgray" : "none"}
               ></Skipped>
+              {/* RED BAR ANIMATION */}
               <Player $isPlaying={isPlaying}></Player>
               <LineWrapper>
                 <Line
-                  $width={"45px"}
-                  $backColor={count >= 1000 ? "lightgray" : "none"}
+                  $width={"50px"}
+                  $backColor={count >= 1 ? "lightgray" : "none"}
                 ></Line>
-                <Line $width={"90px"}></Line>
-                <Line $width={"175px"}></Line>
-                <Line $width={"305px"}></Line>
-                <Line $width={"480px"}></Line>
+                <Line $width={"95px"}></Line>
+                <Line $width={"185px"}></Line>
+                <Line $width={"315px"}></Line>
+                <Line $width={"487px"}></Line>
               </LineWrapper>
             </ProgressBar>
-            <PlayButton onClick={handlePlay}>Play</PlayButton>
+
+            {/* PLAY BUTTON */}
+            <PlayButtonWrapper>
+              <PlayButton onClick={handlePlay}>
+                <PlayButtonImage
+                  src={isPlaying ? stopButtonImage : playButtonImage}
+                  alt="playButton"
+                  $left={isPlaying ? "0px" : "3px"}
+                />
+              </PlayButton>
+            </PlayButtonWrapper>
 
             {/* SUGGESTIONS */}
-            <div>
+            <ButtonSuggestionsWrapper>
               {suggestions
                 ? suggestions.slice(0, 10).map((song, i) => {
                     return (
-                      <button
-                        key={song + i}
-                        onClick={() => {
-                          setInputGuess(song + " ");
-                        }}
-                      >
-                        {song}
-                      </button>
+                      <div key={song + i}>
+                        <div>
+                          <ButtonSuggestions
+                            onClick={() => {
+                              setInputGuess(song + " ");
+                            }}
+                          >
+                            {song}
+                          </ButtonSuggestions>
+                        </div>
+                      </div>
                     );
                   })
                 : ""}
-            </div>
+            </ButtonSuggestionsWrapper>
 
             {/* GUESS INPUT */}
             <div>
@@ -197,9 +238,7 @@ const SongGame = () => {
             {/* SKIP BUTTON */}
             <WrapperSkipSubmit>
               {skip !== 6 ? (
-                <button disabled={isPlaying} onClick={handleSkip}>
-                  S K I P ( + {skip} s )
-                </button>
+                <button onClick={handleSkip}>S K I P ( + {skip} s )</button>
               ) : (
                 <Link to={`/song/${genre}/${song.songName}`}>
                   <button>S K I P</button>
@@ -227,11 +266,6 @@ const Wrapper = styled.div`
   width: 700px;
 `;
 
-const StyledAudio = styled.audio`
-  width: 100%;
-  margin-bottom: 10px;
-`;
-
 const Skipped = styled.div`
   position: absolute;
   width: ${(props) => props.$width};
@@ -243,6 +277,8 @@ const Skipped = styled.div`
 const ProgressBar = styled.div`
   border: 1px solid black;
   height: 15px;
+  margin-top: 200px;
+  margin-bottom: 10px;
 `;
 
 const Player = styled.div`
@@ -273,11 +309,62 @@ const Line = styled.div`
   position: absolute;
   height: 100%;
   width: ${(props) => props.$width};
-  /* background-color: ${(props) => props.$backColor}; */
   border-right: 1px black solid;
 `;
 
-const PlayButton = styled.button``;
+const PlayButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 50px;
+`;
+const PlayButton = styled.button`
+  padding: 10px 12px;
+  border-radius: 50px;
+  cursor: pointer;
+`;
+
+const PlayButtonImage = styled.img`
+  position: relative;
+  left: ${(props) => props.$left};
+  width: 35px;
+`;
+
+const ButtonSuggestionsWrapper = styled.div`
+  position: absolute;
+  bottom: 287px;
+  width: 700px;
+
+  @media (max-height: 768px) {
+    bottom: 102px;
+  }
+
+  @media (max-height: 900px) {
+    bottom: 233px;
+  }
+
+  @media (max-height: 1024px) {
+    bottom: 358px;
+  }
+
+  @media (max-height: 1080px) {
+    bottom: 414px;
+  }
+`;
+const ButtonSuggestionsWrapper2 = styled.div``;
+const ButtonSuggestions = styled.button`
+  display: flex;
+  padding: 7px 0px 7px 20px;
+  width: 700px;
+  height: 33px;
+  border: 1px black solid;
+  border-bottom: 0px;
+  background-color: lightgray;
+  cursor: pointer;
+
+  &:hover {
+    background-color: white;
+  }
+`;
 
 const InputGuess = styled.input`
   width: 675px;
@@ -286,6 +373,7 @@ const InputGuess = styled.input`
 `;
 
 const WrapperSkipSubmit = styled.div`
+  margin-top: 10px;
   display: flex;
   justify-content: space-between;
 `;

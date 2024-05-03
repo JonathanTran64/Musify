464cb24aa0be24884394e51eeaa07cccc20ddfe4
JonathanTran <jonathantran2828@gmail.com>
2024-05-03 19:16:31 -0400

    added media query for guessboxwrapper

--- client/src/pages/SongGame.js
+++ client/src/pages/SongGame.js
@@ -128,8 +128,7 @@ const SongGame = () => {
                           <ButtonSuggestions
                             onClick={() => {
                               setInputGuess(song + " ");
-                            }}
-                          >
+                            }}>
                             {song}
                           </ButtonSuggestions>
                         </div>
@@ -188,6 +187,10 @@ const GuessBoxWrapper = styled.div`
   margin: 0 auto;
   margin-bottom: 200px;
   width: 700px;
+
+  @media (max-width: 1600px) {
+    margin-bottom: 100px;
+  }
 `;
 
 const SecondsPLayButtonWrapper = styled.div`

cde44da5d20422c13758d37364f2363dfd206e8a
Jonathan <jonathantran2828@gmail.com>
2024-05-05 19:57:18 -0400

    change base url axios

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -11,7 +11,7 @@ import { Toaster } from "react-hot-toast";
 import UserProvider from "../context/UserContext";
 import Dashboard from "./Dashboard";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://musify-lac.vercel.app/";
 axios.defaults.withCredentials = true;
 
 const App = () => {

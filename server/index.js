0a46524808287e5137cb324e8fd55347f704b900
Jonathan <jonathantran2828@gmail.com>
2024-05-07 19:30:03 -0400

    changed local to musify

--- server/index.js
+++ server/index.js
@@ -42,7 +42,10 @@ express()
   // })
 
   .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://musify-lac.vercel.app"
+    );
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
